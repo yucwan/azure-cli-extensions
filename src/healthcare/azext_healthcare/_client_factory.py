237a430cbf86bf6724cb3194f1249bb7c99782c0
zikalino <zikalino@microsoft.com>
2019-08-05 13:11:28 +0800

    fix import

--- src/healthcare/azext_healthcare/_client_factory.py
+++ src/healthcare/azext_healthcare/_client_factory.py
@@ -6,8 +6,8 @@
 
 def cf_healthcare(cli_ctx, *_):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    from .vendored_sdks import healthcareapis
-    return get_mgmt_service_client(cli_ctx, healthcareapis)
+    from .vendored_sdks.healthcareapis import HealthcareApisManagementClient
+    return get_mgmt_service_client(cli_ctx, HealthcareApisManagementClient)
 
 
 def cf_services(cli_ctx, *_):
