bcd5ab63e486c23220c6115566cc16d8be52a687
zikalino <zikalino@microsoft.com>
2019-08-05 13:23:34 +0800

    fixing service_descriotion

--- src/healthcare/azext_healthcare/custom.py
+++ src/healthcare/azext_healthcare/custom.py
@@ -24,7 +24,12 @@ def create_healthcare(cmd, client,
                           cosmos_db_configuration=None,
                           authentication_configuration=None,
                           cors_configuration=None):
-    return client.create_or_update(resource_group_name=resource_group, resource_name=name, service_description="Service Description")
+
+    service_description={}
+    service_description=['location'] = location
+    service_description=['kind'] = kind
+    service_description=['access_policies'] = access_policies
+    return client.create_or_update(resource_group_name=resource_group, resource_name=name, service_description=service_description)
 
 
 # module equivalent: azure_rm_healthcareservice
